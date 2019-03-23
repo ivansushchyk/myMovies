4b619b7fe51dc641241b64b9d7a166abb572d377
Ivan <you@example.com>
2019-03-23 20:32:41 +0200

    neimpove view

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $user = 'root';
 $pass = 'root';
 $dbh = new PDO('mysql:host=localhost;dbname=movies', $user, $pass);
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+$result = $dbh->query('SELECT * FROM films');
 ?>
 
 <!DOCTYPE html>
@@ -16,7 +16,7 @@ $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         #middle-element {
             margin: 0 auto;
             text-align: center;
-            width: 1550px;
+            width: 1700px;
         }
 
         #aside {
@@ -25,7 +25,7 @@ $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         }
 
-        #content {
+        #title {
             margin-right: 300px;
             border-top: none;
 
@@ -35,7 +35,7 @@ $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 <body>
 <nav class="navbar navbar-default">
     <a class="navbar-brand" href="#">Add a film</a>
-    <a id="middle-element" class="navbar-brand active" href="#">My movies library </a>
+    <a id="middle-element" class="navbar-brand active" href="#"> My movies library </a>
 </nav>
 <div>
     <div id="aside">
@@ -58,13 +58,39 @@ $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         </form>
     </div>
 
-    <div id="content">
-         <h1 style="margin-left:800px;"> Films list </h1>
-        <hr>
+    <div id="title">
+        <br>
+        <h1 style="margin-left:885px; border: 3px"> Films list </h1>
     </div>
+</div>
+
+<div style="margin-top: 30px">
+<hr>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">Films</th>
+            <th scope="col">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+       <?php foreach ($result as $task): ?>
+        <tr>
+            <th scope="row"> <?php echo $task['title']."(".$task['year'].")"; ?> </th>
+            <td>
+                <a href="tasks/{{ $task->id }}" class="btn btn-primary"> Show </a>
+                <form class="inline-form" method="post" action="tasks/{{$task->id}}">
+                    <input type="hidden" name="_method" value="DELETE">
+                    <input type="submit" class="btn btn-danger" value="Delete">
+                </form>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+
 </div>
 
 
 </body>
 </html>
-
